d77fbe01aa692518f6978cd2da89bda7fd7f72dd
Thayeralexander <thayeralexander@yahoo.com>
2024-10-18 08:25:33 -0700

    first commit

---
+++ src/main/java/project/Project.java
@@ -0,0 +1,14 @@
+package project;
+
+import java.sql.Connection;
+
+public class Project {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+     DbConnection.getConnection();
+    
+
+  }
+
+}
