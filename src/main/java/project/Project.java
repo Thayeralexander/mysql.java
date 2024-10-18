698b5d827ff3f717471eee681de4db4ea191e85b
George Heeres <gheeres@gmail.com>
2024-10-18 12:19:25 -0500

    project: Cleanup / remove binary artifacts. Exclude target folder. fix: Consolidate / remove invalid or unused classes. add: Stub code. work: Reformat / apply indenting.

--- src/main/java/project/Project.java
+++ src/main/java/project/Project.java
@@ -1,14 +1,10 @@
 package project;
-
 import java.sql.Connection;
+import project.dao.JDBCConnection;
 
 public class Project {
-
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-     DbConnection.getConnection();
-    
-
+    Connection connection = JDBCConnection.getConnection();
+    System.out.println("Ready...");
   }
-
 }
