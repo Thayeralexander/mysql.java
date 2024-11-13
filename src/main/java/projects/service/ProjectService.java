676c27a0fb4eb9a39a2da3a11c12f769632a84f4
Thayeralexander <thayeralexander@yahoo.com>
2024-11-13 12:00:35 -0800

    updated repository

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 public class ProjectService {
     
     private ProjectDao projectDao = new ProjectDao();
@@ -21,5 +22,19 @@ public class ProjectService {
         return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
                 "Project with project ID=" + projectId + "does not exist."));
     }
+    public void modifyProjectDetails(Project project) {
+      // TODO Auto-generated method stub
+      if(!projectDao.modifyProjectDetails(project)) {
+        throw new DbException("Project with ID="
+            +project.getProjectId() + " does not exist.");
+      }
+      
+    }
+    
+    public void deleteProject(Integer projectId) {
+      if(!projectDao.deleteProject(projectId)) {
+          throw new DbException("Project With ID=" + projectId + "does not exist.");
+      }
+  }
     
 }
